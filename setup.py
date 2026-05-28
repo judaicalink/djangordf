03a1fcb926febc73f6e701b439785b8237096919
bsesic <Benjamin-777@gmx.de>
2026-05-28 19:03:08 +0200

    Release 0.3.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='djangordf',
     packages=find_packages(include=['djangordf']),
-    version='0.2.0',
+    version='0.3.0',
     description='A RDF library for Django models',
     classifiers=[
         "Programming Language :: Python :: 3",
